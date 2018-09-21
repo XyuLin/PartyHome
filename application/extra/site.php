2691af750ba019f0f5ed06b9883490d522038fbb
lilin <lilinphp@163.com>
2018-09-21 11:37:54 +0800

    site

--- application/extra/site.php
+++ application/extra/site.php
@@ -23,16 +23,4 @@ return array (
   'configgroup' => 
   array (
     'basic' => 'Basic',
-    'email' => 'Email',
-    'dictionary' => 'Dictionary',
-    'user' => 'User',
-    'example' => 'Example',
-  ),
-  'mail_type' => '1',
-  'mail_smtp_host' => 'smtp.qq.com',
-  'mail_smtp_port' => '465',
-  'mail_smtp_user' => '10000',
-  'mail_smtp_pass' => 'password',
-  'mail_verify_type' => '2',
-  'mail_from' => '10000@qq.com',
-);
+  );
